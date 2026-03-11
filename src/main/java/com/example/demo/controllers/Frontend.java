ae11a8ce7c404b1cbf96c6cd1069107aa572f9bc
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-03-11 12:47:11 +0100

    Got the login page working

--- src/main/java/com/example/demo/controllers/Frontend.java
+++ src/main/java/com/example/demo/controllers/Frontend.java
@@ -1,9 +1,11 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,16 +18,41 @@ public class Frontend {
     public String homePage() {
         return "index";
     }
-    // Learning how to get Post data
-    @PostMapping(value = "/api/message")
-    public String message(
-        @RequestParam("name") String userName,
-        @RequestParam("email") String userEmail
-    ) {
-        System.out.println("\n\n\n");
-        System.out.println("User typed name: "+userName);
-        System.out.println("User typed email: "+userEmail);
-        System.out.println("\n\n");
-        return "success";
+    // Learning how to get Form data
+
+    // Using RequestParam
+    @PostMapping("/")
+    public String handleForm(
+        @RequestParam("email") String email,
+        @RequestParam("password") String password,
+        Model model
+    )  {
+        if (email.equals("pogbe@gmail.com") && password.equals("12345")) {
+            return "success";
+        }
+        model.addAttribute("isLoggedIn", false);
+        return "index";
     }
+
+
+
+
+
+
+
+    // Using @ModelAttribute
+    // @PostMapping(value = "/")
+    // public String getForm(
+    //     @ModelAttribute User user,
+    //     Model model
+    // ) {
+    //     String email = user.getEmail();
+    //     String password = user.getPassword();
+    //     if ("pogbe@gmail.com".equals(email) && "12345".equals(password)){
+    //         model.addAttribute("email",email);
+    //         return "success";
+    //     }
+    //     model.addAttribute("isLoggedIn", false);
+    //     return "index";
+    // }
 }
--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,13 +1,18 @@
 package com.example.demo.models;
 
 public class User {
-    private String name;
     private String email;
-
-    public String getName() {
-        return name;
-    }
+    private String password;
     public String getEmail() {
         return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
